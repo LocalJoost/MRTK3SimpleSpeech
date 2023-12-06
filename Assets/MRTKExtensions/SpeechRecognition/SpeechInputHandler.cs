166cc78558800bf63997c5d21654744a4cf89088
Joost van Schaik <joostvanschaik@outlook.com>
2023-12-06 22:30:33 +0100

    Updated to MRTK3 GA and Unity 2022.3.11f1

--- Assets/MRTKExtensions/SpeechRecognition/SpeechInputHandler.cs
+++ Assets/MRTKExtensions/SpeechRecognition/SpeechInputHandler.cs
@@ -1,8 +1,7 @@
-using System;
+using MixedReality.Toolkit;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Microsoft.MixedReality.Toolkit.Input;
 using UnityEngine;
 
 namespace MRTKExtensions.SpeechRecognition
@@ -14,29 +13,29 @@ namespace MRTKExtensions.SpeechRecognition
 
         private void Start()
         {
-            var phraseRecognitionSubsystem = SpeechUtils.GetSubsystem();
+            var phraseRecognitionSubsystem = XRSubsystemHelpers.KeywordRecognitionSubsystem;
             foreach (var phraseAction in phraseActions)
             {
                 if (!string.IsNullOrEmpty(phraseAction.Phrase) && phraseAction.Action.GetPersistentEventCount() > 0)
                 {
-                    phraseRecognitionSubsystem.CreateOrGetEventForPhrase(phraseAction.Phrase).AddListener(() => phraseAction.Action.Invoke());
+                    phraseRecognitionSubsystem.CreateOrGetEventForKeyword(phraseAction.Phrase).AddListener(() => phraseAction.Action.Invoke());
                 }
             }
         }
 
-private void OnValidate()
-{
-    var multipleEntries = phraseActions.GroupBy(p => p.Phrase).Where(p => p.Count() > 1).ToList();
-    if (multipleEntries.Any())
-    {
-        var errorMessage = new StringBuilder();
-        errorMessage.AppendLine("Some phrases defined are more than once , this is not allowed");
-        foreach (var phraseGroup in multipleEntries)
+        private void OnValidate()
         {
-            errorMessage.AppendLine($"- {phraseGroup.Key}");
+            var multipleEntries = phraseActions.GroupBy(p => p.Phrase).Where(p => p.Count() > 1).ToList();
+            if (multipleEntries.Any())
+            {
+                var errorMessage = new StringBuilder();
+                errorMessage.AppendLine("Some phrases defined are more than once , this is not allowed");
+                foreach (var phraseGroup in multipleEntries)
+                {
+                    errorMessage.AppendLine($"- {phraseGroup.Key}");
+                }
+                Debug.LogError(errorMessage);
+            }
         }
-        Debug.LogError(errorMessage);
-    }
-}
     }
 }
